cab26759acb933939b270ac388bc4d6744b36673
simone meneghetti <s.meneghetti7@gmail.com>
2026-02-03 11:47:53 +0100

    aggiunta controller

--- ecommerce/src/main/java/com/java/test/controller/UtenteController.java
+++ ecommerce/src/main/java/com/java/test/controller/UtenteController.java
@@ -2,7 +2,9 @@ package com.java.test.controller;
 
 import com.java.test.dto.UtenteDto;
 import com.java.test.service.IUtenteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,11 +19,11 @@ public class UtenteController {
 	private final IUtenteService service;
 
 	@PostMapping
-	public ResponseEntity<UtenteDto> creazioneUtente(@RequestBody UtenteDto utenteDto)
+	public ResponseEntity<UtenteDto> creazioneUtente(@RequestBody @Valid UtenteDto utenteDto)
 	{
 		UtenteDto utenteCreato = service.creazioneUtente(utenteDto);
 		URI path = ServletUriComponentsBuilder
-				.fromCurrentContextPath()
+				.fromCurrentRequest()
 				.path("/{id}")
 				.buildAndExpand(utenteCreato.utenteId())
 				.toUri();
--- ecommerce/src/main/java/com/java/test/dto/UtenteDto.java
+++ ecommerce/src/main/java/com/java/test/dto/UtenteDto.java
@@ -1,4 +1,12 @@
 package com.java.test.dto;
 
-public record UtenteDto(String nome, String cognome,String email,String codiceFiscale,String utenteId) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record UtenteDto(@NotBlank(message = "Il nome non deve essere vuoto") String nome,
+						@NotBlank(message = "Il cognome non deve essere vuoto") String cognome,
+						@NotBlank(message = "La e-mail non deve essere vuota") @Email(message = "La mail non rispetta i parametri") String email,
+						@Length(min = 16,max = 16,message = "Il codice fiscale deve essere lungo 16") String codiceFiscale,
+						String utenteId) {
 }
