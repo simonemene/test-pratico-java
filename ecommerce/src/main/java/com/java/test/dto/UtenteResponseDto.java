17a510526845a9cea0aa74711656c116ed5809a5
simone meneghetti <s.meneghetti7@gmail.com>
2026-02-03 15:28:10 +0100

    aggiunta test controller

--- ecommerce/src/main/java/com/java/test/dto/UtenteResponseDto.java
+++ ecommerce/src/main/java/com/java/test/dto/UtenteResponseDto.java
@@ -1,6 +1,5 @@
 package com.java.test.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
@@ -9,5 +8,5 @@ public record UtenteResponseDto (@NotBlank(message = "Il nome non deve essere vu
 								@NotBlank(message = "Il cognome non deve essere vuoto") String cognome,
 								@NotBlank(message = "La e-mail non deve essere vuota") @Email(message = "La mail non rispetta i parametri") String email,
 								@Length(min = 16,max = 16,message = "Il codice fiscale deve essere lungo 16") String codiceFiscale,
-								@JsonProperty(access = JsonProperty.Access.READ_ONLY) String utenteId) {
+								String utenteId) {
 }
--- ecommerce/src/test/java/com/java/test/controller/UtenteControllerIntegrationTest.java
+++ ecommerce/src/test/java/com/java/test/controller/UtenteControllerIntegrationTest.java
@@ -1,10 +1,38 @@
 package com.java.test.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.java.test.dto.UtenteRequestDto;
+import com.java.test.dto.UtenteResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UtenteControllerIntegrationTest {
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Test
+	public void creazioneUtente() throws JsonProcessingException {
+		//given
+		UtenteRequestDto request = new UtenteRequestDto("Paolo","Rossi","prova@prova.it","345TGFDCFGTR5462");
+		//when
+		ResponseEntity<UtenteResponseDto> response = template.postForEntity(
+				"/api/utente",request,UtenteResponseDto.class);
+		//then
+		Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		Assertions.assertThat(response.getHeaders().getLocation().getPath())
+				.isEqualTo("/api/utente/"+response.getBody().utenteId());
+
+	}
 
 }
