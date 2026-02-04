6e1a017df8d5ff582f10c3b4d1798c4b314d7d44
simone meneghetti <s.meneghetti7@gmail.com>
2026-02-04 12:41:14 +0100

    controller prodotto

--- ecommerce/src/main/java/com/java/test/controller/ProdottoController.java
+++ ecommerce/src/main/java/com/java/test/controller/ProdottoController.java
@@ -1,4 +1,36 @@
 package com.java.test.controller;
 
+import com.java.test.dto.ProdottoRequestDto;
+import com.java.test.dto.ProdottoResponseDto;
+import com.java.test.service.IProdottoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/prodotto")
 public class ProdottoController {
+
+	private final IProdottoService service;
+
+
+	@PostMapping
+	public ResponseEntity<ProdottoResponseDto> inserisciUnProdotto(@RequestBody
+			ProdottoRequestDto prodotto)
+	{
+		ProdottoResponseDto prodottoCreato = service.inserisciUnProdotto(prodotto);
+		URI path = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(prodotto.prodottoId())
+				.toUri();
+		return ResponseEntity.created(path).body(prodottoCreato);
+	}
 }
--- ecommerce/src/main/java/com/java/test/dto/ProdottoRequestDto.java
+++ ecommerce/src/main/java/com/java/test/dto/ProdottoRequestDto.java
@@ -7,5 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public record ProdottoRequestDto(@NotNull @DecimalMin(value = "0.01", message = "Il prezzo deve essere uguale o maggiore di 0.01") BigDecimal prezzo,
-								 @NotBlank(message = "Il nome del prodotto non può essere vuoto") String nome) {
+								 @NotBlank(message = "Il nome del prodotto non può essere vuoto") String nome,
+								 String prodottoId) {
 }
