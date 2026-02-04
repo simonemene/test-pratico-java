8a52c771e65ecc52def6c43bccf798afaea4f9d9
simone meneghetti <s.meneghetti7@gmail.com>
2026-02-04 08:35:34 +0100

    creazione magazzino

---
+++ ecommerce/src/main/java/com/java/test/entity/StockEntity.java
@@ -0,0 +1,46 @@
+package com.java.test.entity;
+
+import com.java.test.exception.MagazzinoException;
+import jakarta.persistence.*;
+
+@Table(name = "STOCK")
+@Entity
+public class StockEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private int quantita;
+
+	@OneToOne(optional = false)
+	private ProdottoEntity prodotto;
+
+	protected StockEntity()
+	{
+
+	}
+
+	public void collegaProdotto(ProdottoEntity prodotto)
+	{
+		this.prodotto = prodotto;
+	}
+
+
+	public int modificaQuantitaMagazzino(int diminuzione)
+	{
+		int nuovaQuantita = quantita - diminuzione;
+		if(nuovaQuantita <= 0)
+		{
+			throw new MagazzinoException(String.format("Errore prodotto %s, non puoi diminuire la quantità di %s elementi",this.prodotto.getId(),diminuzione));
+		}
+		this.quantita = nuovaQuantita;
+		return this.quantita;
+	}
+
+	public int aumentaQuantitaMagazzino(int aumento)
+	{
+		this.quantita = quantita + aumento;
+		return this.quantita;
+	}
+}
---
+++ ecommerce/src/main/java/com/java/test/exception/MagazzinoException.java
@@ -0,0 +1,8 @@
+package com.java.test.exception;
+
+public class MagazzinoException extends RuntimeException {
+
+	public MagazzinoException(String message) {
+		super(message);
+	}
+}
