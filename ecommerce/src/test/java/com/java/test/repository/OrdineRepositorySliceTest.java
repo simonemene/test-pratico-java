1406cc4242a441da6315a60daee183efd6fa9904
simomene <persolenom@gmail.com>
2026-02-05 18:40:34 +0100

    test ordine

---
+++ ecommerce/src/test/java/com/java/test/repository/OrdineRepositorySliceTest.java
@@ -0,0 +1,28 @@
+package com.java.test.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:sql/service/prodotti/insert-prodotti.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class OrdineRepositorySliceTest {
+
+    @Autowired
+    private OrdineRepository repository;
+
+    @Test
+    public void trovaOrdinePerId()
+    {
+        //given
+
+        //when
+
+        //then
+    }
+}
