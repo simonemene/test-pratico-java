e9d4005d050a777f758bd5bbb7707b490115e179
simone meneghetti <s.meneghetti7@gmail.com>
2026-02-05 17:00:36 +0100

    aggiunti test controller ordine

--- ecommerce/src/test/java/com/java/test/controller/OrdineControllerIntegrationTest.java
+++ ecommerce/src/test/java/com/java/test/controller/OrdineControllerIntegrationTest.java
@@ -1,4 +1,43 @@
 package com.java.test.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.java.test.dto.OrdineEffettuatoResponseDto;
+import com.java.test.dto.OrdineRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Map;
+
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrdineControllerIntegrationTest {
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Sql(scripts = "classpath:sql/service/delete.sql",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(scripts = "classpath:sql/service/ordini/insert-ordine.sql")
+	@Test
+	public void creazioneOrdine() throws JsonProcessingException {
+		//given
+		OrdineRequestDto request = new OrdineRequestDto("dffgdfgfgbfgbgbgbfgb454534",
+				Map.of("dgfgdwwdf454345",2,"rgvbdfgdf454345",1));
+		//when
+		ResponseEntity<OrdineEffettuatoResponseDto> response = template.postForEntity(
+				"/api/ordine",request, OrdineEffettuatoResponseDto.class);
+		//then
+		Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		Assertions.assertThat(response.getHeaders().getLocation().getPath())
+				.isEqualTo("/api/ordine/"+response.getBody().idPubblico());
+
+	}
 }
