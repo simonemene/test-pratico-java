49c51f1d9ee799de50a706f5affe55b2cfa91e55
simone meneghetti <s.meneghetti7@gmail.com>
2026-02-06 12:00:40 +0100

    sistemazione test

--- ecommerce/src/test/java/com/java/test/service/OrdineServiceIntegrationTest.java
+++ ecommerce/src/test/java/com/java/test/service/OrdineServiceIntegrationTest.java
@@ -1,8 +1,7 @@
 package com.java.test.service;
 
 import com.java.test.TestjavaApplicationTests;
-import com.java.test.dto.OrdineEffettuatoResponseDto;
-import com.java.test.dto.ProdottoConQuantitaResponseDto;
+import com.java.test.dto.*;
 import com.java.test.entity.StockEntity;
 import com.java.test.entity.UtenteEntity;
 import com.java.test.repository.OrdineRepository;
@@ -16,6 +15,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 @AutoConfigureTestDatabase
@@ -36,6 +36,7 @@ public class OrdineServiceIntegrationTest extends TestjavaApplicationTests {
 
 	@Transactional
 	@Sql(scripts = "classpath:sql/service/ordini/insert-ordine.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(scripts = "classpath:sql/service/delete.sql",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	@Test
 	public void effettuaOrdine()
 	{
@@ -67,4 +68,60 @@ public class OrdineServiceIntegrationTest extends TestjavaApplicationTests {
 		Assertions.assertThat(stockCaricaBatterie.getQuantita()).isEqualTo(6);
 
 	}
+
+
+	@Transactional
+	@Sql(scripts = "classpath:sql/service/ordini/insert-ordine-singolo.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Test
+	public void ricercaOrdinePerId()
+	{
+		//given
+		String ordineId = "123A";
+		//when
+		OrdineResponseDto response = service.ricercaOrdinePerId(ordineId);
+		//then
+		Assertions.assertThat(response).isNotNull();
+		Assertions.assertThat(response.movimenti()).isNotEmpty();
+		Assertions.assertThat(response.movimenti().size()).isEqualTo(1);
+		List<MovimentoResponseDto> movimenti = response.movimenti();
+		MovimentoResponseDto movimento = movimenti.stream().filter(mov->mov.nomeProdotto().equals("CARICA CELLULARE")).findFirst().get();
+		Assertions.assertThat(movimento.quantitaOrdinata()).isEqualTo(3);
+
+		Assertions.assertThat(response.idPubblicoOrdine()).isEqualTo("123A");
+		Assertions.assertThat(response.utenteId()).isEqualTo("dffgdfgfgbfgbgbgbfgb454534");
+	}
+
+	@Transactional
+	@Sql(scripts = "classpath:sql/service/ordini/insert-ordine-completo.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Test
+	public void ricercaTuttiGliOrdini()
+	{
+		//given
+		//when
+		OrdiniResponseDto ordini = service.ricercaTuttiGliOrdini();
+		//then
+		Assertions.assertThat(ordini.ordini().size()).isEqualTo(2);
+		OrdineResponseDto ordine = ordini.ordini().stream().filter(ord->ord.idPubblicoOrdine().equals("123A")).findFirst().get();
+		Assertions.assertThat(ordine.movimenti()).isNotEmpty();
+		Assertions.assertThat(ordine.movimenti().size()).isEqualTo(1);
+		List<MovimentoResponseDto> movimenti = ordine.movimenti();
+		MovimentoResponseDto movimento = movimenti.stream().filter(mov->mov.nomeProdotto().equals("CARICA CELLULARE")).findFirst().get();
+		Assertions.assertThat(movimento.quantitaOrdinata()).isEqualTo(3);
+
+		Assertions.assertThat(ordine.idPubblicoOrdine()).isEqualTo("123A");
+		Assertions.assertThat(ordine.utenteId()).isEqualTo("dffgdfgfgbfgbgbgbfgb454534");
+
+		OrdineResponseDto ordine2 = ordini.ordini().stream().filter(ord->ord.idPubblicoOrdine().equals("564W")).findFirst().get();
+		Assertions.assertThat(ordine2.movimenti()).isNotEmpty();
+		Assertions.assertThat(ordine2.movimenti().size()).isEqualTo(2);
+		List<MovimentoResponseDto> movimenti2 = ordine2.movimenti();
+		MovimentoResponseDto movimento1 = movimenti2.stream().filter(mov->mov.nomeProdotto().equals("COMPUTER")).findFirst().get();
+		Assertions.assertThat(movimento1.quantitaOrdinata()).isEqualTo(10);
+		MovimentoResponseDto movimento2 = movimenti2.stream().filter(mov->mov.nomeProdotto().equals("CELLULARE")).findFirst().get();
+		Assertions.assertThat(movimento2.quantitaOrdinata()).isEqualTo(2);
+
+		Assertions.assertThat(ordine2.idPubblicoOrdine()).isEqualTo("564W");
+		Assertions.assertThat(ordine2.utenteId()).isEqualTo("dffgdfgfgbfttttgb454534");
+	}
+
 }
