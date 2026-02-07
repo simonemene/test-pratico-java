3321cfe7f6e2d66a5a5be5f63734f5f76060f622
Simone Meneghetti <s.meneghetti7@gmail.com>
2026-02-07 17:22:10 +0100

    configurazioni sicurezza

--- ecommerce/src/main/java/com/java/test/configuration/SecurityConfiguration.java
+++ ecommerce/src/main/java/com/java/test/configuration/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package com.java.test.configuration;
 
+import com.java.test.repository.UtenteRepository;
+import com.java.test.security.CustomAuthenticationProvider;
+import com.java.test.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,6 +38,18 @@ public class SecurityConfiguration {
 	@Bean
 	public CompromisedPasswordChecker compromisedPasswordChecker() {
 		return new HaveIBeenPwnedRestApiPasswordChecker();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider(UserDetailsService service)
+	{
+		return new CustomAuthenticationProvider(service,passwordEncoder());
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(UtenteRepository repository)
+	{
+		return new CustomUserDetailsService(repository);
 	}
 
 
