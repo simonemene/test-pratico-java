c321231c70a94ab91eb0c37846a92282306631ee
Simone Meneghetti <s.meneghetti7@gmail.com>
2026-02-10 07:23:53 +0100

    sistemazione test

--- ecommerce/src/test/java/com/java/test/testcontainer/ConcorrenzaEcommerceTest.java
+++ ecommerce/src/test/java/com/java/test/testcontainer/ConcorrenzaEcommerceTest.java
@@ -7,21 +7,20 @@ import com.java.test.repository.StockRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
-
-
-@ActiveProfiles("container")
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
 @Sql(scripts = "classpath:sql/service/delete.sql",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @Sql(scripts = "classpath:sql/service/ordini/ordini-concorrenza.sql",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Import({TestContainerConfiguration.class})
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ConcorrenzaEcommerceTest {
 
     private static final String PRODOTTO_ID = "dgfgdfgdf45mnbv";
@@ -33,7 +32,7 @@ public class ConcorrenzaEcommerceTest {
     private StockRepository stockRepository;
 
     @Test
-    void optimistic_lock_scoppia_sicuramente_con_due_transazioni() {
+    void testConcorrenza() {
 
         TransactionTemplate tx1 = new TransactionTemplate(transactionManager);
         TransactionTemplate tx2 = new TransactionTemplate(transactionManager);
